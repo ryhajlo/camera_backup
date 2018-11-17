0f0a1d0a279625ebed82ea05d534d5335fbe70e9
Nick Ryhajlo <nick.ryhajlo@gmail.com>
2018-11-16 19:32:57 -0800

    Added temperature from I2C

--- camera_backup.py
+++ camera_backup.py
@@ -3,10 +3,11 @@
 import sys
 import os
 import glob
+import pigpio
 from os import listdir
 from os.path import isfile, join
 import subprocess
-from Adafruit_IO import Client
+from Adafruit_IO import Client, RequestError
 import base64
 
 def main(args):
@@ -19,14 +20,33 @@ def main(args):
         print "Folder Path: " + folder_path + " doesn't exist, exiting."
         raise ValueError("Incorrect Parameters")
 
-    print "Grabbing files from " + folder_path
-
-    # Get the list of files in that directory
-    pictures = glob.glob(join(folder_path, '2*.jpg')) # Use the leading 2 to prevent us from getting 'latest.jpg'
-    videos = glob.glob(join(folder_path, '*.avi'))
-    latest_picture = max(pictures, key=os.path.getctime)
-    print "The latest picture is: " + latest_picture
-
+    aio = Client('ryhajlo', 'b5fe0936d9a84629a2d49cd45858fc67')
+    temperature = get_arduino_temperature(0x08)
+    if temperature and temperature < 150 and temperature > -150:
+        print "Temperature: " + str(temperature)
+        try:
+            aio.send('external-temperature', str(temperature))
+        except RequestError:
+            print "Cannot send data"
+    else:
+        print "No temperature read"
+    
+    # Start handling pictures
+    videos = get_videos(folder_path)
+    pictures = get_pictures(folder_path)
+    if pictures:
+        latest_picture = max(pictures, key=os.path.getctime)
+        print "The latest picture is: " + latest_picture
+        with open(latest_picture, "rb") as imageFile:
+            image_str = base64.b64encode(imageFile.read())
+  
+        print "Uploading latest to Adafruit IO"
+        aio.send('pic', image_str )
+        print "Finished uploading to Adafruit IO"
+    else:
+        latest_picture = None
+        print "No pictures"
+    
     # Upload the files to dropbox
     # Build our command to upload files
     command = []
@@ -50,15 +70,6 @@ def main(args):
     subprocess.call(command)
     print "Finished uploading videos"
 
-    # Upload the latest picture to Adafruit IO
-    aio = Client('ryhajlo', 'b5fe0936d9a84629a2d49cd45858fc67')
-    with open(latest_picture, "rb") as imageFile:
-        str = base64.b64encode(imageFile.read())
-  
-    print "Uploading latest to Adafruit IO"
-    aio.send('pic', str )
-    print "Finished uploading to Adafruit IO"
-
     # Now that everything is uploaded, delete it all
     for picture in pictures:
         print "Deleting " + picture
@@ -66,6 +77,36 @@ def main(args):
     for video in videos:
         print "Deleting " + video
         os.remove(video)
+    
+def get_videos(folder_path):
+    videos = glob.glob(join(folder_path, '*.avi'))
+    return videos
+
+def get_pictures(folder_path):
+    print "Grabbing files from " + folder_path
+
+    # Get the list of files in that directory
+    pictures = glob.glob(join(folder_path, '2*.jpg')) # Use the leading 2 to prevent us from getting 'latest.jpg'
+    latest_picture = max(pictures, key=os.path.getctime)
+
+    return pictures
+
+def get_arduino_temperature(device_address):
+    """Read temperature from arduino"""
+    pi = pigpio.pi()
+    h = pi.i2c_open(1, device_address)
+
+    pi.i2c_write_device(h, [0x01])
+    (count, data) = pi.i2c_read_device(h, 2)
+    pi.i2c_close(h)
+
+    if count >= 2:
+        raw_temperature = data[1] << 8
+        raw_temperature |= data[0]
+
+        return (raw_temperature/10.0) * 1.8 + 32.0
+    else:
+        return None
 
 if __name__ == "__main__":
     main(sys.argv[1:])
