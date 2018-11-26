2f408eef20c16e78bae646cfb79397fd1ba4fb01
Nick Ryhajlo <nick.ryhajlo@gmail.com>
2018-11-25 17:32:07 -0800

    Adding ccs811

--- read_sensors.py
+++ read_sensors.py
@@ -1,38 +1,58 @@
 #!/usr/bin/python3
+import time
 import board
 import busio
 import digitalio
-import pigpio
+import smbus
 import sys
 import adafruit_bmp280
+import adafruit_ccs811
 from Adafruit_IO import Client, RequestError
 
 def main(args):
     aio = Client('ryhajlo', 'b5fe0936d9a84629a2d49cd45858fc67')
+    
+    i2c_bus = busio.I2C(board.SCL, board.SDA)
+    ccs811_sensor = adafruit_ccs811.CCS811(i2c_bus)
+    
+    while not ccs811_sensor.data_ready:
+        pass
+    temp = ccs811_sensor.temperature
+    ccs811_sensor.temp_offset = temp - 25.0
+    print("CCS811 temperature offset: " + str(temp))
+    # Read three times
+    for x in range(0, 3):
+        print("Reading " + str(x))
+        while not ccs811_sensor.data_ready:
+            pass
+        eco2 = ccs811_sensor.eco2
+        tvoc = ccs811_sensor.tvoc
+        temperature = ccs811_sensor.temperature
+        time.sleep(0.5)
+    while True:
+        print("Gathering Data")
+        gather_data(aio, ccs811_sensor)
+        print("Sleeping for 300 seconds")
+        time.sleep(300)
 
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    cs = digitalio.DigitalInOut(board.D4)
-    sensor = adafruit_bmp280.Adafruit_BMP280_SPI(spi, cs)
+def gather_data(aio, ccs811_sensor):
+    (temperature, pressure) = get_bmp280_data()
 
-    temperature = sensor.temperature * 1.8 + 32.0
-    pressure = sensor.pressure
-
-    if temperature < 120 and temperature > 20:
+    if temperature < 120 and temperature > 20 and pressure > 950 and pressure < 1070:
         try:
             aio.send('internal-temperature', temperature)
         except RequestError:
             print("Cannot send temperature data")
 
-    if pressure > 950 and pressure < 1070:
         try:
             aio.send('pressure', pressure)
         except RequestError:
             print("Cannot send temperature data")
 
-    print('Internal Temperature: {} degrees F'.format(temperature)) 
+    print('Internal Temperature: {} degrees F'.format(temperature))
     print('Pressure: {}hPa'.format(pressure))
-        
-    temperature = get_arduino_temperature(0x08)
+
+    temperature = get_arduino_temperature(0x10)
     if temperature and temperature < 150 and temperature > 20:
         print("Temperature: " + str(temperature))
         try:
@@ -43,23 +63,60 @@ def main(args):
         print("No temperature read")
 
     print('External Temperature: {} degrees F'.format(temperature)) 
+    
+    (eco2, tvoc, temperature) = get_ccs811_data(ccs811_sensor)
+    if temperature and temperature < 150 and temperature > 20:
+        print("Temperature: " + str(temperature))
+        try:
+            aio.send('ccs811-temperature', str(temperature))
+            aio.send('ccs811-eco2', str(eco2))
+            aio.send('ccs811-tvoc', str(tvoc))
+        except RequestError:
+            print("Cannot send data")
+    print("CO2: %1.0f PPM" % eco2)
+    print("TVOC: %1.0f PPM" % tvoc)
+    print("Temp: %0.1f C" % temperature)
+
+def get_bmp280_data():
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    cs = digitalio.DigitalInOut(board.D4)
+    bmp280_sensor = adafruit_bmp280.Adafruit_BMP280_SPI(spi, cs)
+    print("Reading temperature from BMP280")
+    temperature = bmp280_sensor.temperature * 1.8 + 32.0
+    print("Reading pressure from BMP280")
+    pressure = bmp280_sensor.pressure
+    return (temperature, pressure)
+
+def get_ccs811_data(ccs811_sensor):
+    """Read from the CCS811"""
+    print("Reading CCS811")
+    
+    # Read three times
+    for x in range(0, 3):
+        print("Reading " + str(x))
+        while not ccs811_sensor.data_ready:
+            pass
+        eco2 = ccs811_sensor.eco2
+        tvoc = ccs811_sensor.tvoc
+        temperature = ccs811_sensor.temperature
+        time.sleep(0.5)
+    while not ccs811_sensor.data_ready:
+        pass
+    eco2 = ccs811_sensor.eco2
+    tvoc = ccs811_sensor.tvoc
+    temperature = ccs811_sensor.temperature * 1.8 + 32.0
+    print("CO2: %1.0f PPM" % eco2)
+    print("TVOC: %1.0f PPM" % tvoc)
+    print("Temp: %0.1f F" % temperature)
+    
+    return (eco2, tvoc, temperature)
 
 def get_arduino_temperature(device_address):
     """Read temperature from arduino"""
-    pi = pigpio.pi()
-    h = pi.i2c_open(1, device_address)
-
-    pi.i2c_write_device(h, [0x01])
-    (count, data) = pi.i2c_read_device(h, 2)
-    pi.i2c_close(h)
-
-    if count >= 2:
-        raw_temperature = data[1] << 8
-        raw_temperature |= data[0]
-
-        return (raw_temperature/10.0) * 1.8 + 32.0
-    else:
-        return None
+    print("Reading arduino temperature")
+    bus=smbus.SMBus(1)
+    raw_temperature = bus.read_word_data(device_address, 0x01)
+    return (raw_temperature/10.0) * 1.8 + 32.0
 
 if __name__ == "__main__":
     main(sys.argv[1:])
