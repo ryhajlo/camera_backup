17b15126946a8358f73f14e86ae5e81e0c1ede96
Nick Ryhajlo <nick.ryhajlo@gmail.com>
2018-12-10 18:05:24 -0800

    Updated camera backup to handle both camera types. Also upload to dropbox first, then resize for adafruit IO

--- camera_backup.py
+++ camera_backup.py
@@ -20,46 +20,61 @@ def main(args):
         print "Folder Path: " + folder_path + " doesn't exist, exiting."
         raise ValueError("Incorrect Parameters")
 
+    #Get the camera name. Default to visible
+    try:
+        with open('camera_name.cfg') as f:
+            camera_name = f.readLine()
+    except:
+        camera_name = 'visible'
+
     aio = Client('ryhajlo', 'b5fe0936d9a84629a2d49cd45858fc67')
     
     # Start handling pictures
     videos = get_videos(folder_path)
     pictures = get_pictures(folder_path)
     if pictures:
+        # Upload the files to dropbox
+        # Build our command to upload files
+        command = []
+        command.append('/home/pi/Dropbox-Uploader/dropbox_uploader.sh')
+        command.append('upload')
+        for picture in pictures:
+            print "Will upload: " + picture
+            command.append(picture)
+        command.append('/camera/pictures/' + camera_name + '/')
+        subprocess.call(command)
+        print "Finished uploading pictures"
+
+        # Do the same for videos
+        command = []
+        command.append('/home/pi/Dropbox-Uploader/dropbox_uploader.sh')
+        command.append('upload')
+        for video in videos:
+            print "Will upload: " + video
+            command.append(video)
+        command.append('/camera/videos/' + camera_name + '/')
+        subprocess.call(command)
+        print "Finished uploading videos"
+
+        command = []
+        command.append("mogrify")
+        command.append("-resize")
+        command.append("320x240")
+        command.append("/var/lib/motion/*.jpg")
+        subprocess.call(command)
+
         latest_picture = max(pictures, key=os.path.getctime)
         print "The latest picture is: " + latest_picture
         with open(latest_picture, "rb") as imageFile:
             image_str = base64.b64encode(imageFile.read())
   
         print "Uploading latest to Adafruit IO"
-        aio.send('pic', image_str )
+        aio.send('pic-' + camera_name, image_str )
         print "Finished uploading to Adafruit IO"
     else:
         latest_picture = None
         print "No pictures"
     
-    # Upload the files to dropbox
-    # Build our command to upload files
-    command = []
-    command.append('/home/pi/Dropbox-Uploader/dropbox_uploader.sh')
-    command.append('upload')
-    for picture in pictures:
-        print "Will upload: " + picture
-        command.append(picture)
-    command.append('/camera/pictures/noir/')
-    subprocess.call(command)
-    print "Finished uploading pictures"
-
-    # Do the same for videos
-    command = []
-    command.append('/home/pi/Dropbox-Uploader/dropbox_uploader.sh')
-    command.append('upload')
-    for video in videos:
-        print "Will upload: " + video
-        command.append(video)
-    command.append('/camera/videos/noir/')
-    subprocess.call(command)
-    print "Finished uploading videos"
 
     # Now that everything is uploaded, delete it all
     for picture in pictures:
